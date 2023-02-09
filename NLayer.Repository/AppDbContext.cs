b9f69558ce427b492a3c80ce00ff1f547e30ab48
Ebubekir Nayman <ebubekirn96@gmail.com>
2023-02-09 20:03:00 +0300

    AppDbContext eklendi ve migartion yapıldı.

---
+++ NLayer.Repository/AppDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly()); // ApplyConfigurationsFromAssembly IEntityTypeConfiguration interface ine sahip configuration class ları assembly içerisinden otomatik olarak çekecek. 
+
+            modelBuilder.Entity<ProductFeature>().HasData(new ProductFeature()
+            {
+                Id= 1,
+                Color= "Kırmızı",
+                Height= 100,
+                Width= 200,
+                ProductId= 1
+            },
+            new ProductFeature()
+            {
+                Id = 2,
+                Color = "Mavi",
+                Height = 300,
+                Width = 500,
+                ProductId = 2
+            }); // Bu şekilde direk context içinde de seed tanımlayabiliriz. Uygun olanı farklı bir class ta tanımlamaktır.
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ NLayer.Repository/Configurations/CategoryConfiguraiton.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Configurations
+{
+    internal class CategoryConfiguraiton : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id); // Id primary key olsun dedik
+            builder.Property(x => x.Id).UseIdentityColumn(); // Id birer birer artsın dedik.
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(50); // Name null olmasın ve max 50 karakter olsun dedik.
+            builder.ToTable("Categories"); // Tablonun ismini belirtiyoruz. Defauşt olarak AppDbContext teki adı alıyor. 
+        }
+    }
+}
